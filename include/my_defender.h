fbbfb47b4ecd94177ab6cb7ccb4e623c9bd203ce
mariuscontoli <marius.contoli-legay@epitech.eu>
2020-02-12 15:46:01 +0100

    directories added

---
+++ include/my_defender.h
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2020
+** my_defender.h
+** File description:
+** my defender header file
+*/
+
+#ifndef MY_DEFENDER_H_
+#define MY_DEFENDER_H_
+
+#include <SFML/Audio.h>
+#include <SFML/Graphics.h>
+#include <SFML/Graphics/RectangleShape.h>
+#include <SFML/Window.h>
+#include <SFML/Window/Export.h>
+#include <SFML/System/Export.h>
+#include <SFML/System/Time.h>
+#include <SFML/System/Types.h>
+#include <SFML/Window/Types.h>
+#include <SFML/System/Vector2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <math.h>
+
+#endif /* !MY_DEFENDER_H_ */
