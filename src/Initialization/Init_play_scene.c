f8209b64b4541c274cca40f404ca831b161c23a3
cram <julien.augugliaro@epitech.eu>
2020-02-19 14:56:14 +0100

    Added maps

--- src/Initialization/Init_play_scene.c
+++ src/Initialization/Init_play_scene.c
@@ -7,12 +7,31 @@
 
 #include "../../include/my_defender.h"
 
+void fill_coord_one(map *map)
+{
+    sfVector2f list[] = 
+}
+
+void i_map_coord(map *map)
+{
+    switch (map->map_index) {
+        case 1 : fill_coord_one(map);
+            break;
+        case 2 : fill_coord_two(map);
+            break;
+        case 3 : fill_coord_three(map);
+            break;
+        default :
+            break;
+    }
+}
+
 void i_map(play_scene *play_scene)
 {
     play_scene->map.sprite = sfSprite_create();
     play_scene->map.pos = (sfVector2f){500, 500};
     play_scene->difficulty = EASY;
-    // play_scene->map.coord
+
 }
 
 void i_play_scene(play_scene *play_scene, sfRenderWindow *window)
