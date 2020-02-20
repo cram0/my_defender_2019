dd4621130b3fc7cdfc969e1bff20eb19878a4d38
marius <marius.contoli-legay@epitech.eu>
2020-02-20 14:21:55 +0100

    added choice menu background with track thumbnails

--- src/Initialization/Init_play_scene.c
+++ src/Initialization/Init_play_scene.c
@@ -7,24 +7,24 @@
 
 #include "../../include/my_defender.h"
 
-void fill_coord_one(map *map)
-{
-    sfVector2f list[] = {{655, 0}, {621, 93}, {255, 104}, {250, 645}, {850, 643}, {850, 330}, {661, 326}, {644, 524}, {454, 521}, {447, 218}, {1049, 221}, {1049, 762}, {669, 775}, {665, 878}};
-}
-
-void i_map_coord(map *map)
-{
-    switch (map->map_index) {
-        case 1 : fill_coord_one(map);
-            break;
-        case 2 : fill_coord_two(map);
-            break;
-        case 3 : fill_coord_three(map);
-            break;
-        default :
-            break;
-    }
-}
+//void fill_coord_one(map *map)
+//{
+//    sfVector2f list[] = {{655, 0}, {621, 93}, {255, 104}, {250, 645}, {850, 643}, {850, 330}, {661, 326}, {644, 524}, {454, 521}, {447, 218}, {1049, 221}, {1049, 762}, {669, 775}, {665, 878}};
+//}
+//
+//void i_map_coord(map *map)
+//{
+//    switch (map->map_index) {
+//        case 1 : fill_coord_one(map);
+//            break;
+//        case 2 : fill_coord_two(map);
+//            break;
+//        case 3 : fill_coord_three(map);
+//            break;
+//        default :
+//            break;
+//    }
+//}
 
 void i_map(play_scene *play_scene)
 {
