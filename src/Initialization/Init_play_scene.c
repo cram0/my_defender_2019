aca9a258179bc1c23516ae63eba363830ecf80ac
cram <julien.augugliaro@epitech.eu>
2020-02-20 14:23:36 +0100

    added add coord func

--- src/Initialization/Init_play_scene.c
+++ src/Initialization/Init_play_scene.c
@@ -7,6 +7,11 @@
 
 #include "../../include/my_defender.h"
 
+void add_coord(map *map)
+{
+
+}
+
 void fill_coord_one(map *map)
 {
     sfVector2f list[] = {{655, 0}, {621, 93}, {255, 104}, {250, 645}, {850, 643}, {850, 330}, {661, 326}, {644, 524}, {454, 521}, {447, 218}, {1049, 221}, {1049, 762}, {669, 775}, {665, 878}};
@@ -31,7 +36,6 @@ void i_map(play_scene *play_scene)
     play_scene->map.sprite = sfSprite_create();
     play_scene->map.pos = (sfVector2f){500, 500};
     play_scene->difficulty = EASY;
-
 }
 
 void i_play_scene(play_scene *play_scene, sfRenderWindow *window)
