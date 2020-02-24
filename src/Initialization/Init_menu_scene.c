b5b1b5862fa494eca24c15f8d02e83a671661efa
cram <julien.augugliaro@epitech.eu>
2020-02-24 17:03:59 +0100

    Fixed background, changed main menu's appearance

--- src/Initialization/Init_menu_scene.c
+++ src/Initialization/Init_menu_scene.c
@@ -40,14 +40,14 @@ button create_button(char *file_path, sfFloatRect rect)
 
 void i_menu_ui(menu_ui *menu_ui)
 {
-    menu_ui->play_btn = create_button("img/buttons/play_", (sfFloatRect){50, 600, 285, 60});
+    menu_ui->play_btn = create_button("img/buttons/play_", (sfFloatRect){800, 400, 285, 60});
     sfSprite_setPosition(menu_ui->play_btn.sprite,  menu_ui->play_btn.pos);
-    menu_ui->hiscore_btn = create_button("img/buttons/highscores_", (sfFloatRect){50, 700, 285, 60});
+    menu_ui->hiscore_btn = create_button("img/buttons/highscores_", (sfFloatRect){800, 500, 285, 60});
     sfSprite_setPosition(menu_ui->hiscore_btn.sprite, menu_ui->hiscore_btn.pos);
-    menu_ui->settings_btn = create_button("img/buttons/settings_", (sfFloatRect){50, 800, 285, 60});
+    menu_ui->settings_btn = create_button("img/buttons/settings_", (sfFloatRect){800, 600, 285, 60});
     sfSprite_setPosition(menu_ui->settings_btn.sprite,
     menu_ui->settings_btn.pos);
-    menu_ui->quit_btn = create_button("img/buttons/quit_", (sfFloatRect){50, 900, 285, 60});
+    menu_ui->quit_btn = create_button("img/buttons/quit_", (sfFloatRect){800, 700, 285, 60});
     sfSprite_setPosition(menu_ui->quit_btn.sprite,  menu_ui->quit_btn.pos);
 }
 
