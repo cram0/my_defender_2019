f7a2f602fa6c7223ffc1a74eb86937ff9a3d394d
marius <marius.contoli-legay@epitech.eu>
2020-02-25 12:48:36 +0100

    fixed choice menu check functions

--- src/zMain/check_hover_click.c
+++ src/zMain/check_hover_click.c
@@ -19,10 +19,8 @@ void check_hover_click_ui(menu_scene *menu_scene)
         menu_scene->ui.quit_btn.state = CLICK;
 }
 
-void check_hover_click_opt(menu_scene *menu_scene)
+void check_hover_click_choice_menu(menu_scene *menu_scene)
 {
-    if (menu_scene->opt_ui.close_button.state == HOVER && sfMouse_isButtonPressed(sfMouseLeft) && menu_scene->choice_state == 1)
-        menu_scene->opt_ui.close_button.state = CLICK;
     if (menu_scene->choice_menu.close_btn.state == HOVER && sfMouse_isButtonPressed(sfMouseLeft) && menu_scene->choice_state == 1)
         menu_scene->choice_menu.close_btn.state = CLICK;
     if (menu_scene->choice_menu.map_one_btn.state == HOVER && sfMouse_isButtonPressed(sfMouseLeft) && menu_scene->choice_state == 1) {
@@ -34,10 +32,17 @@ void check_hover_click_opt(menu_scene *menu_scene)
     if (menu_scene->choice_menu.map_three_btn.state == HOVER && sfMouse_isButtonPressed(sfMouseLeft) && menu_scene->choice_state == 1) {
         *menu_scene->map_index = 3;
     }
+
+}
+void check_hover_click_options_menu(menu_scene *menu_scene)
+{
+    if (menu_scene->opt_ui.close_button.state == HOVER && sfMouse_isButtonPressed(sfMouseLeft) && menu_scene->choice_state == 1)
+        menu_scene->opt_ui.close_button.state = CLICK;
 }
 
 void check_hover_click(menu_scene *menu_scene)
 {
     check_hover_click_ui(menu_scene);
-    check_hover_click_opt(menu_scene);
+    check_hover_click_options_menu(menu_scene);
+    check_hover_click_choice_menu(menu_scene);
 }
