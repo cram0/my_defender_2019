db8f48ccef169c38053baef2f29657092d697bcf
marius <marius.contoli-legay@epitech.eu>
2020-03-10 14:23:30 +0100

    separating init music and sound in a new file

--- src/Initialization/Init_game_core.c
+++ src/Initialization/Init_game_core.c
@@ -7,27 +7,6 @@
 
 #include "../../include/my_defender.h"
 
-void i_menu_music(menu_scene *scene)
-{
-    scene->music = sfMusic_createFromFile("music/main_theme.ogg");
-    sfMusic_setVolume(scene->music, 20.0);
-    sfMusic_play(scene->music);
-    sfMusic_setLoop(scene->music, sfTrue);
-}
-
-void i_sound_buttons(game_core *game_core)
-{
-    game_core->menu_scene.sound_buffer = sfSoundBuffer_createFromFile("music/soft_click.wav");
-    game_core->menu_scene.sound = sfSound_create();
-    sfSound_setBuffer(game_core->menu_scene.sound, game_core->menu_scene.sound_buffer);
-}
-
-void i_music(game_core *game_core)
-{
-    i_sound_buttons(game_core);
-    i_menu_music(&game_core->menu_scene);
-}
-
 void i_game_core(game_core *game_core)
 {
     sfRenderWindow *window = sfRenderWindow_create((sfVideoMode){1920, 1080, 32}
