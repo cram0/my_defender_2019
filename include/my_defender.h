8f6137f5c2b83e30f2fda05cf9d5ff2281be65ea
marius <marius.contoli-legay@epitech.eu>
2020-03-14 22:27:06 +0100

    modified proto

--- include/my_defender.h
+++ include/my_defender.h
@@ -305,7 +305,7 @@ void add_coord(map *map, sfVector2f list[], int len);
 void add_coord_node(coord *node, sfVector2f pos, int index);
 void change_texture_dnd(play_scene *scene);
 void change_origin_dnd(play_scene *scene);
-int is_the_turret_in_zones(play_scene *scene);
+int is_the_cursor_in_zones(play_scene *scene);
 bool is_mouse_in_map(play_scene *scene);
 
 //GET
