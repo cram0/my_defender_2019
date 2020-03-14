fcd01390e465ed462ee26112782244a2e4414fb2
marius <marius.contoli-legay@epitech.eu>
2020-03-14 17:34:55 +0100

    fix display order

--- src/Display/Disp_game_core.c
+++ src/Display/Disp_game_core.c
@@ -167,10 +167,10 @@ void d_play_scene(play_scene *play_scene)
    d_play_map(play_scene);
    d_play_dragndrop(play_scene);
    d_turret_placed(play_scene);
+   d_waves(play_scene);
    d_play_hud(play_scene);
    d_price_dnd(play_scene);
    d_player_infos(play_scene);
-   d_waves(play_scene);
    d_pause_menu(play_scene);
 }
 
